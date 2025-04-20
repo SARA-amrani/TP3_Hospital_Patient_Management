d3e41ce0babb9e11c08d9b44a544092cda91dd92
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-04-20 13:08:46 +0100

    Création du formulaire pour gerer les patients

--- src/main/java/ma/enset/tp3_springmvc_hopital/web/PatientController.java
+++ src/main/java/ma/enset/tp3_springmvc_hopital/web/PatientController.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.List;
 
@@ -41,5 +42,18 @@ public class PatientController {
     @GetMapping("/")
     public String home() {
         return "redirect:/index";
+    }
+
+    @GetMapping("/patients")
+    @ResponseBody
+    public List<Patient> listPatients() {
+        return patientRepository.findAll();
+    }
+
+    // methode qui permet de retourner une vue
+    @GetMapping("/formPatient")
+    public String formPatient(Model model) {
+        model.addAttribute("patient", new Patient());
+        return "formPatient";
     }
 }
