06dab59053f891f19c781230ffed0d4cc0809e86
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-04-20 18:32:40 +0100

    Sécurité avec Spring security :InMemomy Authentication

---
+++ src/main/java/ma/enset/tp3_springmvc_hopital/security/SecurityConfig.java
@@ -0,0 +1,42 @@
+package ma.enset.tp3_springmvc_hopital.security;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    //definire les utilisateurs qui peuvent acceder a l'application
+
+    @Bean
+    public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
+        return new InMemoryUserDetailsManager(
+                User.withUsername("user1").password(passwordEncoder.encode("12345")).roles("USER").build(),
+                User.withUsername("user2").password(passwordEncoder.encode("12345")).roles("USER").build(),
+                User.withUsername("admin").password(passwordEncoder.encode("12345")).roles("USER","ADMIN").build()
+        );
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .authorizeHttpRequests(auth -> auth
+                        .anyRequest().authenticated()
+                )
+                .formLogin(Customizer.withDefaults()) // Active le formulaire de connexion
+                .build();
+    }
+}
+ // @Bean : chaque methode utilise Bean c a d il va s'execute au demarrage
