74b791236a6ac9ed6c4022255979cbd6836d24be
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-05-06 15:53:06 +0100

    UPDATE README

--- src/main/java/ma/enset/tp3_springmvc_hopital/HopitalApplication.java
+++ src/main/java/ma/enset/tp3_springmvc_hopital/HopitalApplication.java
@@ -7,8 +7,10 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
 import java.util.Date;
 
@@ -29,6 +31,10 @@ public class HopitalApplication implements CommandLineRunner {
         //patientRepository.save(new Patient(null, "HOUDA","EL AMRANI", new Date(), false,70));
         //patientRepository.save(new Patient(null, "MUSTAPHA","EL AMRANI", new Date(), true,60));
 
+
+
+
+
         // en utilisant Builder
         Patient patient=Patient.builder()
                 .nom("Imane")
@@ -44,5 +50,18 @@ public class HopitalApplication implements CommandLineRunner {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+
+    CommandLineRunner commandLineRunner(JdbcUserDetailsManager jdbcUserDetailsManager) {
+        PasswordEncoder passwordEncoder = passwordEncoder();
+        return args -> {
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("user11").password(passwordEncoder.encode("1234")).roles("USER").build()
+            );
+            jdbcUserDetailsManager.createUser(
+                    User.withUsername("admin11").password(passwordEncoder.encode("1234")).roles("USER","ADMIN").build()
+            );
+
+        };
     }
 }
--- src/main/java/ma/enset/tp3_springmvc_hopital/security/SecurityConfig.java
+++ src/main/java/ma/enset/tp3_springmvc_hopital/security/SecurityConfig.java
@@ -11,7 +11,10 @@ import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 @EnableWebSecurity
@@ -22,7 +25,14 @@ public class SecurityConfig {
 
     //definire les utilisateurs qui peuvent acceder a l'application
 
-    @Bean
+    //JDBC Authentication
+  // @Bean
+    public JdbcUserDetailsManager jdbcUserDetailsManager(DataSource dataSource) {
+       return new JdbcUserDetailsManager(dataSource);
+
+    }
+
+   // @Bean
     public InMemoryUserDetailsManager inMemoryUserDetailsManager() {
         return new InMemoryUserDetailsManager(
                 User.withUsername("user1").password(passwordEncoder.encode("12345")).roles("USER").build(),
@@ -46,6 +56,7 @@ public class SecurityConfig {
                 )
                 .formLogin(form -> form
                         .loginPage("/login")
+                        .defaultSuccessUrl("/")
                         .permitAll()
                 )
                 .build();
