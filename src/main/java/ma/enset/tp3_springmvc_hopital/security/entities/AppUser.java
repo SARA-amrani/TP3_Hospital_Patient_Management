97958ac51683478a491af70947775bc6f3529dbc
SARA-amrani <172842309+SARA-amrani@users.noreply.github.com>
2025-05-08 13:27:10 +0100

    Utilisation de UserDetailsService: creation de la classe AppUser.java

---
+++ src/main/java/ma/enset/tp3_springmvc_hopital/security/entities/AppUser.java
@@ -0,0 +1,8 @@
+package ma.enset.tp3_springmvc_hopital.security.entities;
+
+public class AppUser {
+    private String userId;
+    private String userName;
+    private String password;
+    private String email;
+}
